3d58ec5d0c24a2259a50b7bf7f6d20fa5b025af8
Minhluu2911 <minh.luu291101@hcmut.edu.vn>
2021-12-04 17:24:10 +0700

    adding description

--- app.py
+++ app.py
@@ -4,7 +4,10 @@ st.set_page_config(
     layout="wide"
 )
 
-st.title('NEURAL STYLE TRANSFER NETWORK')
+st.title('NEURAL STYLE TRANSFER: CREATING YOUR ART WITH DEEP LEARNING')
+st.write("""
+ ***Description***: Neural style transfer is an optimization technique used to take your image and a style reference (such as an artwork by a famous painter) — and blend them together such that the input image is transformed to look like the style of the style image.
+         """)
 
 col1, col2 = st.columns(2)
 
